993f6462f8e08da679b4e8ea5c5f0edc6f7c6069
Mikołaj Pich <m.pich@outlook.com>
2018-03-04 00:16:27 +0100

    Remove method to get urls for substitution indexes

--- src/Service/SubstitutionUrlsGenerator.php
+++ src/Service/SubstitutionUrlsGenerator.php
@@ -12,25 +12,10 @@ class SubstitutionUrlsGenerator
 
         foreach (explode(',', getenv('SUBSTITUTIONS_DATE_SCHEMAS')) as $key => $value) {
             $urls[] = sprintf(
-                '%1$s/plan/zastepstwa/%2$s/plany/%3$s.html',
-                rtrim(getenv('TIMETABLE_HOST'), '/'),
+                '%1$s/%2$s/plany/%3$s.html',
+                rtrim(getenv('SUBSTITUTIONS_BASE_URL'), '/'),
                 $date->format($value),
                 getenv('TIMETABLE_SYMBOL')
-            );
-        }
-
-        return $urls;
-    }
-
-    public function getUrlsForIndex(DateTime $date): array
-    {
-        $urls = [];
-
-        foreach (explode(',', getenv('SUBSTITUTIONS_DATE_SCHEMAS')) as $key => $value) {
-            $urls[] = sprintf(
-                '%1$s/plan/zastepstwa/%2$s',
-                rtrim(getenv('TIMETABLE_HOST'), '/'),
-                $date->format($value)
             );
         }
 
